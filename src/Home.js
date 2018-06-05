3a62377fd029fe2a376264e9d9c2d1b3692449bb
Tomas <tomizechsterson@gmail.com>
2018-06-04 21:57:56 -0700

    Convert Home to function component
    Definitely doesn't need to be a class

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <p>To get started, select a game above.</p>
-            </div>
-        );
-    }
-}
+const Home = () => {
+    return (
+        <div>
+            <p>To get started, select a game above.</p>
+        </div>
+    );
+};
 export default Home;

3c8842393e326045b834d3873ccbab47dee13f2e
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2018-06-08 15:28:31 -0700

    Add table rows
    Also clean up a couple of names and go back to using localhost for server data

--- src/ADD2/ADD2CharacterTable.js
+++ src/ADD2/ADD2CharacterTable.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react';
 
 export default class ADD2CharacterTable extends Component {
-//    constructor(props) {
-//        super(props);
-//    }
+    constructor(props) {
+        super(props);
+
+        ADD2CharacterTable.createRows = ADD2CharacterTable.createRows.bind(this);
+    }
+
+    static createRows(item) {
+        return <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.playedBy}</td>
+            <td>No</td>
+        </tr>
+    }
 
     render() {
         const tableStyle = {
-            width: "100%"
+            width: '100%'
         };
+
+        const entries = this.props.characters;
+        const rowItems = entries.map(ADD2CharacterTable.createRows);
 
         return (
             <table style={tableStyle}>
                 <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Played By</th>
-                        <th>Completed?</th>
-                    </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Played By</th>
+                    <th>Completed?</th>
+                </tr>
                 </thead>
+                <tbody>
+                {rowItems}
+                </tbody>
             </table>
         );
     }
--- src/ADD2/ADD2Characters.js
+++ src/ADD2/ADD2Characters.js
@@ -1,11 +1,12 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
 import ADD2CharacterTable from './ADD2CharacterTable';
 
 const testData = [
     {id: 1, name: 'Biff', playedBy: 'someone', completionStep: 2, str: 18, dex: 14, con: 9, int: 16, wis: 11, chr: 12},
     {id: 2, name: 'Gristle', playedBy: 'someone else', completionStep: 1, str: 0, dex: 0, con: 0, int: 0, wis: 0, chr: 0},
     {id: 3, name: 'Crunch', playedBy: 'person', completionStep: 1, str: 0, dex: 0, con: 0, int: 0, wis: 0, chr: 0},
-    {id: 4, name: 'Rip', playedBy: 'another person', completionStep: 2, str: 12, dex: 12, con: 10, int: 6, wis: 18, chr: 15}
+    {id: 4, name: 'Rip', playedBy: 'another person', completionStep: 2, str: 12, dex: 12, con: 10, int: 6, wis: 18, chr: 15},
+    {id: 5, name: 'Swift', playedBy: 'Generic McRandom', completionStep: 2, str: 12, dex: 12, con: 10, int: 6, wis: 18, chr: 15}
 ];
 
 const useTestData = true;
@@ -14,20 +15,20 @@ export default class ADD2Characters extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            characterData: []
         };
     }
 
     loadCharsFromServer() {
         if(useTestData)
-            this.setState({data: testData});
+            this.setState({characterData: testData});
         else
         {
             const xhr = new XMLHttpRequest();
-            xhr.open('get', 'https://add20-dev.azurewebsites.net/api/add2character', true);
+            xhr.open('get', 'http://localhost:42000/api/add2character', true);
             xhr.onload = function() {
-                const data = JSON.parse(xhr.responseText);
-                this.setState({data: data});
+                const responseData = JSON.parse(xhr.responseText);
+                this.setState({characterData: responseData});
             }.bind(this);
             xhr.send();
         }
@@ -40,13 +41,13 @@ export default class ADD2Characters extends Component {
     render() {
         const topLevelStyle = {
             columnCount: 2,
-            columnRuleStyle: "solid"
+            columnRuleStyle: 'solid'
         };
 
         return (
             <div style={topLevelStyle}>
-                <ADD2CharacterTable characters={this.state.data} />
-                <p className="selectedADD2Column">Selected Character Details</p>
+                <ADD2CharacterTable characters={this.state.characterData} />
+                <p>Selected Character Details</p>
             </div>
         );
     }

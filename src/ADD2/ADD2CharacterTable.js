3eb65fa12451a0a1d91dbaa53314efec3b770600
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2018-06-12 16:30:05 -0700

    Update Completed column to use completionStep
    Resolves #4

--- src/ADD2/ADD2CharacterTable.js
+++ src/ADD2/ADD2CharacterTable.js
@@ -3,12 +3,13 @@ import './ADD2CharacterTable.css';
 
 const ADD2CharacterTable = (props) => {
 
+    const completeStep = 2;
     const entries = props.characters;
     const rowItems = entries.map(function(item) {
         return <tr key={item.id} onClick={() => props.onCharacterSelect(item.id)}>
             <td>{item.name}</td>
             <td>{item.playedBy}</td>
-            <td>No</td>
+            <td>{item.completionStep === completeStep ? 'Yes' : 'No'}</td>
         </tr>
     });
 

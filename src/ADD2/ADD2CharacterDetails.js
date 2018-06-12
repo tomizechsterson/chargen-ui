c49035200e4ff0b66e16e3e100e325bd92884a5e
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2018-06-12 16:58:58 -0700

    Add complete step to details component

--- src/ADD2/ADD2CharacterDetails.js
+++ src/ADD2/ADD2CharacterDetails.js
@@ -3,12 +3,14 @@ import React from 'react';
 const ADD2CharacterDetails = (props) => {
     const {selectedChar} = props;
     const isOneSelected = Boolean(selectedChar);
+    const completeStep = 2;
 
     return (
         <div>
-            {isOneSelected && <div>
+            {isOneSelected && selectedChar.completionStep === completeStep && <div>
                 <button type="button" onClick={() => props.onDelete(selectedChar.id)}>Delete</button>
                 <p>{selectedChar.name}</p></div>}
+            {isOneSelected && selectedChar.completionStep < completeStep && <div>Creation component</div>}
             {!isOneSelected && <p>No character selected</p>}
         </div>
     );

50feafa0531e0966c38e5c4e533eec0d70bc3dfe
Tomas <tomizechsterson@gmail.com>
2018-06-12 22:15:50 -0700

    Slightly improve formatting of VitalsDisplay

--- src/ADD2/VitalsDisplay.js
+++ src/ADD2/VitalsDisplay.js
@@ -4,8 +4,11 @@ const VitalsDisplay = (props) => {
     const {selectedChar} = props;
     return (
         <div>
-            <p>Race: {selectedChar.race} Gender: {selectedChar.gender} Class: {selectedChar.className} Alignment: {selectedChar.alignment} <br/>
-            Age: {selectedChar.age} Height: {selectedChar.height} Weight: {selectedChar.weight}</p>
+            <p>
+                Race: {selectedChar.race} Gender: {selectedChar.gender} Class: {selectedChar.className} Alignment: {selectedChar.alignment}
+                <br/>
+                Age: {selectedChar.age} Height: {selectedChar.height} Weight: {selectedChar.weight}
+            </p>
         </div>
     );
 };

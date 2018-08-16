8b07284d28ce18cd57e219ab3cacffbf1685a0e6
Tomas <tomizechsterson@gmail.com>
2018-08-16 14:13:16 -0700

    Deconstruct props; looks much cleaner

--- src/StatRollRules/AssignmentControl.js
+++ src/StatRollRules/AssignmentControl.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const AssignmentControl = (props) => {
+    const {stat, charStat, roll, rollStyle} = props;
     return (
         <div>
-            <input type='button' onClick={() => props.onSelectStat(props.stat)} value={props.stat} /> {props.charStat}
-            {props.roll && <input type='button' onClick={() => props.onSelectRoll(props.roll)} value={props.roll.value} disabled={props.roll.assigned} />}
-            {props.roll && <span style={props.rollStyle}>{props.roll.text}</span>}
+            <input type='button' onClick={() => props.onSelectStat(stat)} value={stat} /> {charStat}
+            {roll && <input type='button' onClick={() => props.onSelectRoll(roll)} value={roll.value} disabled={roll.assigned} />}
+            {roll && <span style={rollStyle}>{roll.text}</span>}
         </div>
     );
 };

a42a99860a6cd5877ce9eb93c71ed11d86f0be96
Tomas <tomizechsterson@gmail.com>
2018-08-17 15:28:19 -0700

    Run npm test before running npm build
    Also add a new test for ADD2DisplayCompleted

--- src/ADD2/__tests__/ADD2DisplayCompletedTests.js
+++ src/ADD2/__tests__/ADD2DisplayCompletedTests.js
@@ -3,8 +3,15 @@ import {shallow} from 'enzyme';
 import ADD2DisplayCompleted from '../ADD2DisplayCompleted';
 
 describe('ADD2DisplayCompleted Tests', () => {
-    it('renders at least one div', () => {
+    it('always renders at least one div', () => {
         const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
         expect(component.find('div').length).toBeGreaterThan(0);
     });
+
+    it('always renders an `h4` with the character name', () => {
+        const component = shallow(<ADD2DisplayCompleted selectedChar={{name: 'test'}}/>);
+        expect(component.find('h4').text()).toEqual('test');
+    });
+
+    it('always renders ')
 });

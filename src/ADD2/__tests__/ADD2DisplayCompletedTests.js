e570c23ac3146fb3dc33972c64d09cf37369dea4
Tomas <tomizechsterson@gmail.com>
2018-08-17 15:34:57 -0700

    Fix failing test

--- src/ADD2/__tests__/ADD2DisplayCompletedTests.js
+++ src/ADD2/__tests__/ADD2DisplayCompletedTests.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ADD2DisplayCompleted from '../ADD2DisplayCompleted';
 import VitalsDisplay from "../../GeneralDisplays/VitalsDisplay";
+import StatsDisplay from "../../GeneralDisplays/StatsDisplay";
+import SavingThrowsDisplay from "../../GeneralDisplays/SavingThrowsDisplay";
 
 describe('ADD2DisplayCompleted Tests', () => {
     it('always renders at least one div', () => {
@@ -16,6 +18,16 @@ describe('ADD2DisplayCompleted Tests', () => {
 
     it('always renders a VitalsDisplay', () => {
         const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
-        expect(component.find(VitalsDisplay).length).toBe(2);
+        expect(component.find(VitalsDisplay).length).toBe(1);
+    });
+
+    it('always renders a StatsDisplay', () => {
+        const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
+        expect(component.find(StatsDisplay).length).toBe(1);
+    });
+
+    it('always renders a SavingThrowsDisplay', () => {
+        const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
+        expect(component.find(SavingThrowsDisplay).length).toBe(1);
     });
 });

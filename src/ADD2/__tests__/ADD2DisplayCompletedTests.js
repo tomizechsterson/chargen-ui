f0914c86f3c8bb044632a9a8e3a5d409d014d654
Tomas <tomizechsterson@gmail.com>
2018-08-17 17:00:09 -0700

    Complete tests for ADD2DisplayCompleted component
    Delete button render and onDelete prop with id. Also replace the toBe(1) calls with toBeDefined() calls since that doesn't generate a silly warning and should test the same thing

--- src/ADD2/__tests__/ADD2DisplayCompletedTests.js
+++ src/ADD2/__tests__/ADD2DisplayCompletedTests.js
@@ -12,23 +12,29 @@ describe('ADD2DisplayCompleted Tests', () => {
     });
 
     it('always renders an `h4` with the character name', () => {
-        const component = shallow(<ADD2DisplayCompleted selectedChar={{name: 'test'}}/>);
-        expect(component.find('h4').text()).toEqual('test');
+        const component = shallow(<ADD2DisplayCompleted selectedChar={{name: 'testName'}}/>);
+        expect(component.find('h4').text()).toEqual('testName');
     });
 
     it('always renders a VitalsDisplay', () => {
         const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
-        expect(component.find(VitalsDisplay).length).toBe(1);
+        expect(component.find(VitalsDisplay)).toBeDefined();
     });
 
     it('always renders a StatsDisplay', () => {
         const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
-        expect(component.find(StatsDisplay).length).toBe(1);
+        expect(component.find(StatsDisplay)).toBeDefined();
     });
 
     it('always renders a SavingThrowsDisplay', () => {
         const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
-        expect(component.find(SavingThrowsDisplay).length).toBe(1);
+        expect(component.find(SavingThrowsDisplay)).toBeDefined();
+    });
+
+    it('always renders the delete button', () => {
+        const component = shallow(<ADD2DisplayCompleted selectedChar={{}}/>);
+        expect(component.find('button')).toBeDefined();
+        expect(component.find('button').text()).toEqual('Delete');
     });
 
     describe('when selectedChar is defined', () => {
@@ -47,6 +53,18 @@ describe('ADD2DisplayCompleted Tests', () => {
         it('sets the rendered `VitalsDisplay`s `selectedChar` prop to the same value as `testChar`', () => {
             const savingThrowsDisplay = shallow(<ADD2DisplayCompleted selectedChar={testChar}/>).find(SavingThrowsDisplay);
             expect(savingThrowsDisplay.props().selectedChar).toBe(testChar);
+        });
+    });
+
+    describe('delete button', () => {
+        it('calls delete with selected character id when clicked', () => {
+            const deleteFunc = jest.fn();
+            const component = shallow(<ADD2DisplayCompleted onDelete={deleteFunc} selectedChar={{id: 1}}/>);
+            const button = component.find('button').first();
+
+            button.simulate('click');
+
+            expect(deleteFunc).toBeCalledWith(1);
         });
     });
 });

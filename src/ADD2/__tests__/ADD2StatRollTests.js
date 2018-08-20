f1e1271ecedb7caa8c379d279bf9700350a221f6
Tomas <tomizechsterson@gmail.com>
2018-08-19 22:34:25 -0700

    Minor refactoring for roll rule drop down tests

--- src/ADD2/__tests__/ADD2StatRollTests.js
+++ src/ADD2/__tests__/ADD2StatRollTests.js
@@ -12,8 +12,7 @@ describe('ADD2StatRoll', () => {
     });
 
     describe('roll rule drop down', () => {
-        let component;
-        let dropDown;
+        let component, rollRuleDropDown;
         const assertComponents = (rollOnce, rollTwice, assignment, rollFour, addSeven) => {
             expect(component.find(RollOnce)).toHaveLength(rollOnce);
             expect(component.find(RollTwice)).toHaveLength(rollTwice);
@@ -24,53 +23,53 @@ describe('ADD2StatRoll', () => {
 
         beforeEach(() => {
             component = shallow(<ADD2StatRoll/>);
-            dropDown = component.find('select');
+            rollRuleDropDown = component.find('select');
         });
 
         it('changes state to selected roll rule', () => {
-            dropDown.simulate('change', {target: {value: 'testRule'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'testRule'}});
             expect(component.state().rollRule).toBe('testRule');
         });
 
         it('renders only RollOnce component if selected', () => {
-            dropDown.simulate('change', {target: {value: 'rollOnce'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'rollOnce'}});
             assertComponents(1, 0, 0, 0, 0);
         });
 
         it('renders only RollTwice component if selected', () => {
-            dropDown.simulate('change', {target: {value: 'rollTwice'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'rollTwice'}});
             assertComponents(0, 1, 0, 0, 0);
         });
 
         it('renders only Assignment component if selected', () => {
-            dropDown.simulate('change', {target: {value: 'assignment'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'assignment'}});
             assertComponents(0, 0, 1, 0, 0);
         });
 
         it('renders only Assignment component if 2x is selected', () => {
-            dropDown.simulate('change', {target: {value: 'assignment2x'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'assignment2x'}});
             assertComponents(0, 0, 1, 0, 0);
         });
 
         it('renders only RollFour component if selected', () => {
-            dropDown.simulate('change', {target: {value: 'roll4'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'roll4'}});
             assertComponents(0, 0, 0, 1, 0);
         });
 
         it('renders only Add7 component if selected', () => {
-            dropDown.simulate('change', {target: {value: 'add7Dice'}});
+            rollRuleDropDown.simulate('change', {target: {value: 'add7Dice'}});
             assertComponents(0, 0, 0, 0, 1);
         });
 
         describe('when one of the assignment rules is selected', () => {
             it('false is passed as double prop to Assignment when single is selected', () => {
-                dropDown.simulate('change', {target: {value: 'assignment'}});
+                rollRuleDropDown.simulate('change', {target: {value: 'assignment'}});
                 const assignment = component.find(Assignment);
                 expect(assignment.props().double).toBeFalsy();
             });
 
             it('true is passed as double prop to Assignment when 2x is selected', () => {
-                dropDown.simulate('change', {target: {value: 'assignment2x'}});
+                rollRuleDropDown.simulate('change', {target: {value: 'assignment2x'}});
                 const assignment = component.find(Assignment);
                 expect(assignment.props().double).toBeTruthy();
             });

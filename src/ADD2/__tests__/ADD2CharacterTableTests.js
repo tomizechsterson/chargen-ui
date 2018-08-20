7fb6d58d9cd432e0f506d849a695667f20230278
Tomas <tomizechsterson@gmail.com>
2018-08-19 23:15:00 -0700

    Add a couple missing unit tests for ADD2CharacterTable
    Overlooked the calling of the onSelect prop function properly and the rendering of no rows if there's no characters

--- src/ADD2/__tests__/ADD2CharacterTableTests.js
+++ src/ADD2/__tests__/ADD2CharacterTableTests.js
@@ -15,12 +15,30 @@ describe('ADD2CharacterTable', () => {
             expect(component.find('tbody tr')).toHaveLength(2);
         });
 
+        it('renders no rows if there is no data', () => {
+            const testData = [];
+            const component = shallow(<ADD2CharacterTable characters={testData}/>);
+            expect(component.find('tbody tr')).toHaveLength(0);
+        }); // TODO: Maybe consider rendering a <p> tag with a message
+
         it('renders Yes or No depending on if the character is completed', () => {
-            const testData = [{id: 1, completionStep: 2}, {id: 2, completionStep:1}];
+            const testData = [{id: 1, completionStep: 2}, {id: 2, completionStep: 1}];
             const component = shallow(<ADD2CharacterTable characters={testData}/>);
 
             expect(component.find('tbody tr').at(0).find('td').at(3).text()).toEqual('Yes');
             expect(component.find('tbody tr').at(1).find('td').at(3).text()).toEqual('No');
+        });
+
+        it('calls onSelect with the proper id when a row is clicked', () => {
+            const testData = [{id: 1}, {id: 2}];
+            const select = jest.fn();
+            const component = shallow(<ADD2CharacterTable characters={testData} onSelect={select}/>);
+            const entryRows = component.find('tbody tr');
+
+            entryRows.at(0).simulate('click');
+            expect(select).toBeCalledWith(1);
+            entryRows.at(1).simulate('click');
+            expect(select).toBeCalledWith(2);
         });
     });
 });

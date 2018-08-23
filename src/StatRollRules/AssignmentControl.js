606cba4bc1f66ae92076e04455b62f1c4618eb23
Tomas <tomizechsterson@gmail.com>
2018-08-22 20:55:17 -0700

    Get some missed deconstructing

--- src/StatRollRules/AssignmentControl.js
+++ src/StatRollRules/AssignmentControl.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AssignmentControl = (props) => {
-    const {stat, charStat, rolls} = props;
+    const {stat, charStat, rolls, onSelectStat, onSelectRoll} = props;
     const rollStyle = {
         color: '#999'
     };
@@ -9,8 +9,8 @@ const AssignmentControl = (props) => {
         const roll = rolls[0];
         return (
             <div>
-                <input type='button' onClick={() => props.onSelectStat(stat)} value={stat} /> {charStat}
-                {roll && <input type='button' onClick={() => props.onSelectRoll(roll)} value={roll.value} disabled={roll.assigned} />}
+                <input type='button' onClick={() => onSelectStat(stat)} value={stat} /> {charStat}
+                {roll && <input type='button' onClick={() => onSelectRoll(roll)} value={roll.value} disabled={roll.assigned} />}
                 {roll && <span style={rollStyle}>{roll.text}</span>}
             </div>
         );
@@ -18,10 +18,10 @@ const AssignmentControl = (props) => {
     else {
         return (
             <div>
-                <input type='button' onClick={() => props.onSelectStat(stat)} value={stat} /> {charStat}
-                {rolls[0] && <input type='button' onClick={() => props.onSelectRoll(rolls[0])} value={rolls[0].value} disabled={rolls[0].assigned} />}
+                <input type='button' onClick={() => onSelectStat(stat)} value={stat} /> {charStat}
+                {rolls[0] && <input type='button' onClick={() => onSelectRoll(rolls[0])} value={rolls[0].value} disabled={rolls[0].assigned} />}
                 {rolls[0] && <span style={rollStyle}>{rolls[0].text}</span>}
-                {rolls[1] && <input type='button' onClick={() => props.onSelectRoll(rolls[1])} value={rolls[1].value} disabled={rolls[1].assigned} />}
+                {rolls[1] && <input type='button' onClick={() => onSelectRoll(rolls[1])} value={rolls[1].value} disabled={rolls[1].assigned} />}
                 {rolls[1] && <span style={rollStyle}>{rolls[1].text}</span>}
             </div>
         );

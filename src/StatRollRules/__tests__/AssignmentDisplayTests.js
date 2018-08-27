494baf079c0dbb2872fc0bfa81e55c62863b2070
Tomas <tomizechsterson@gmail.com>
2018-08-26 21:46:43 -0700

    Finish AssignmentDisplay tests
    This should be plenty good enough

--- src/StatRollRules/__tests__/AssignmentDisplayTests.js
+++ src/StatRollRules/__tests__/AssignmentDisplayTests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import AssignmentDisplay from '../AssignmentDisplay';
 import AssignmentControl from "../AssignmentControl";
 
@@ -19,6 +19,42 @@ describe('AssignmentDisplay tests', () => {
         component.setState({selectedStat: 'test', selectedRoll: {value: '99'}});
         expect(component.find('div').text()).toContain('Selected Stat: test');
         expect(component.find('div').text()).toContain('Selected Roll: 99');
+    });
+
+    describe('AssignmentControl components', () => {
+        let component, assignmentControl, testRolls;
+
+        beforeEach(() => {
+            testRolls = getTestRolls();
+            component = mount(<AssignmentDisplay rolls={testRolls} selectedChar={{}}/>);
+            assignmentControl = component.find(AssignmentControl).at(0);
+        });
+
+        it('selects expected stat', () => {
+            assignmentControl.find('input').at(0).simulate('click');
+            expect(component.state().selectedStat).toBe('STR');
+        });
+
+        it('deselects expected stat', () => {
+            component.setState({selectedStat: 'STR'});
+            assignmentControl.find('input').at(0).simulate('click');
+            expect(component.state().selectedStat).toBe('');
+        });
+
+        it('selects expected roll', () => {
+            assignmentControl.find('input').at(1).simulate('click');
+            expect(component.state().selectedRoll.id).toBe(1);
+        });
+
+        it('deselects expected roll', () => {
+            component.setState({selectedRoll: testRolls[0]});
+            assignmentControl.find('input').at(1).simulate('click');
+            expect(component.state().selectedRoll).toBeUndefined();
+        });
+
+        const getTestRolls = () => {
+            return [{id: 1, value: 3, assigned: false}];
+        };
     });
 
     describe('Assign button', () => {
@@ -41,6 +77,53 @@ describe('AssignmentDisplay tests', () => {
             component.setState({selectedStat: 'test', selectedRoll: {}});
             expect(component.find('input').at(0).html()).not.toContain('disabled=""');
         });
+
+        describe('Assigns stats and clears selected stat and roll', () => {
+            it('str', () => {
+                const component = assignStat('STR');
+                assertStatAssignment(component, component.instance().props.selectedChar.str);
+            });
+
+            it('dex', () => {
+                const component = assignStat('DEX');
+                assertStatAssignment(component, component.instance().props.selectedChar.dex);
+            });
+
+            it('con', () => {
+                const component = assignStat('CON');
+                assertStatAssignment(component, component.instance().props.selectedChar.con);
+            });
+
+            it('int', () => {
+                const component = assignStat('INT');
+                assertStatAssignment(component, component.instance().props.selectedChar.int);
+            });
+
+            it('wis', () => {
+                const component = assignStat('WIS');
+                assertStatAssignment(component, component.instance().props.selectedChar.wis);
+            });
+
+            it('chr', () => {
+                const component = assignStat('CHR');
+                assertStatAssignment(component, component.instance().props.selectedChar.chr);
+            });
+
+            const assignStat = (stat) => {
+                const testRolls = [{id: 1, value: 3, assigned: false}];
+                const component = shallow(<AssignmentDisplay selectedChar={{}} rolls={testRolls}/>);
+                component.setState({selectedRoll: testRolls[0], selectedStat: stat});
+                component.find('input').at(0).simulate('click');
+                return component;
+            };
+
+            const assertStatAssignment = (component, selectedCharStat) => {
+                expect(selectedCharStat).toBe(3);
+                expect(component.instance().props.rolls[0].assigned).toBeTruthy();
+                expect(component.state().selectedStat).toBe('');
+                expect(component.state().selectedRoll).toBeUndefined();
+            };
+        });
     });
 
     describe('Reset button', () => {
@@ -52,36 +135,51 @@ describe('AssignmentDisplay tests', () => {
         });
 
         it('is enabled when str has been assigned', () => {
-            assertResetButton({str: 3});
+            assertResetButtonEnabled({str: 3});
         });
 
         it('is enabled when dex has been assigned', () => {
-            assertResetButton({dex: 3})
+            assertResetButtonEnabled({dex: 3})
         });
 
         it('is enabled when con has been assigned', () => {
-            assertResetButton({con: 3})
+            assertResetButtonEnabled({con: 3})
         });
 
         it('is enabled when int has been assigned', () => {
-            assertResetButton({int: 3});
+            assertResetButtonEnabled({int: 3});
         });
 
         it('is enabled when wis has been assigned', () => {
-            assertResetButton({wis: 3});
+            assertResetButtonEnabled({wis: 3});
         });
 
         it('is enabled when chr has been assigned', () => {
-            assertResetButton({chr: 3});
+            assertResetButtonEnabled({chr: 3});
         });
 
-        const assertResetButton = (testChar) => {
+        const assertResetButtonEnabled = (testChar) => {
             const component = shallow(<AssignmentDisplay rolls={[]} selectedChar={testChar}/>);
             expect(component.find('input').at(1).html()).not.toContain('disabled=""');
         };
 
         it('resets all stats to undefined and all rolls to unassigned when clicked', () => {
+            const component = shallow(<AssignmentDisplay
+                rolls={[{assigned: true}, {assigned: true}, {assigned: true}, {assigned: true}, {assigned: true}, {assigned: true}]}
+                selectedChar={{str: 3, dex: 3, con: 3, int: 3, wis: 3, chr: 3}}/>);
 
+            component.find('input').at(1).simulate('click');
+
+            const testChar = component.instance().props.selectedChar;
+            expect(testChar.str).toBeUndefined();
+            expect(testChar.dex).toBeUndefined();
+            expect(testChar.con).toBeUndefined();
+            expect(testChar.int).toBeUndefined();
+            expect(testChar.wis).toBeUndefined();
+            expect(testChar.chr).toBeUndefined();
+            component.instance().props.rolls.forEach((roll) => {
+                expect(roll.assigned).toBeFalsy();
+            });
         });
     });
 });

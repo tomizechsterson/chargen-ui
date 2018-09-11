e4a1aae372207e685c2163b1ed1f23821d26fd6e
Tomas <tomizechsterson@gmail.com>
2018-09-10 21:49:37 -0700

    Slight rename for stat adjustment display component test

--- src/GeneralDisplays/__tests__/StatAdjustmentDisplayTests.js
+++ src/GeneralDisplays/__tests__/StatAdjustmentDisplayTests.js
@@ -3,7 +3,7 @@ import {shallow} from 'enzyme';
 import StatAdjustmentDisplay from '../StatAdjustmentDisplay';
 
 describe('Stat Adjustment Display tests', () => {
-    it('renders expected stat and adjustment', () => {
+    it('renders expected text, stat, and adjustment', () => {
         const component = shallow(<StatAdjustmentDisplay text={'TEST'} stat={99} adjustment={-1}/>);
         expect(component.text()).toContain('TEST');
         expect(component.text()).toContain('99');

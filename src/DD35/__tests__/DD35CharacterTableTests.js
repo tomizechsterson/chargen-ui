da0e300e3ffae0dfddd57eb143d3985c44bab596
Tomas <tomizechsterson@users.noreply.github.com>
2019-03-17 18:58:25 -0700

    Add new assertion for deleting DD35 characters
    Should probably also make sure the expected one actually got removed

--- src/DD35/__tests__/DD35CharacterTableTests.js
+++ src/DD35/__tests__/DD35CharacterTableTests.js
@@ -127,6 +127,7 @@ describe('DD35 Character Table Tests', () => {
 
             rows.at(0).find('button').at(1).simulate('click');
             expect(component.state().characterData).toHaveLength(1);
+            expect(component.state().characterData[0].id).toBe(2);
         });
 
         it('calls the server with the expected id', () => {

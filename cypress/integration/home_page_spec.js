c1015fe8108cf7e5e6f6a5f5cd6523e4905c050b
Tomas <tomizechsterson@users.noreply.github.com>
2019-06-29 21:33:36 -0700

    Add assert for url on home page spec

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,7 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
         cy.visit('/');
+        cy.url().should('equal', Cypress.config('baseUrl'));
         cy.contains('Home');
         cy.contains('AD&D 2nd Edition');
         cy.contains('D&D 3.5');

114b6d0f5a896448497eaa2a10c3a482aa20038d
Tomas <tomizechsterson@users.noreply.github.com>
2019-07-20 00:21:43 -0700

    Change a couple URL checks to location pathname checks

--- cypress/integration/ADD2_HappyPath_spec.js
+++ cypress/integration/ADD2_HappyPath_spec.js
@@ -2,7 +2,7 @@ describe('AD&D 2nd Edition Happy Path Without Stubbing', () => {
     it('Can create and delete an AD&D 2nd Edition character', () => {
         cy.visit('/');
         cy.get('.selectorHeader > li > a[href="/add2"]').click();
-        cy.url().should('includes', '/add2');
+        cy.location('pathname').should('eq', '/add2');
         cy.createTestCharacter();
         cy.chooseRollRuleAndRollStats('rollTwice');
         cy.get('[style="color: rgb(153, 153, 153);"]');
--- cypress/integration/DD35_HappyPath_spec.js
+++ cypress/integration/DD35_HappyPath_spec.js
@@ -2,7 +2,7 @@ describe('D&D 3.5 Happy Path Without Stubbing', () => {
     it('Can create and delete a D&D 3.5 Character', () => {
         cy.visit('/');
         cy.get('.selectorHeader > li > a[href="/dd35"]').click();
-        cy.url().should('includes', '/dd35');
+        cy.location('pathname').should('eq', '/dd35');
         cy.get('input').type('cypress test').should('have.value', 'cypress test');
         cy.get('button').contains('Create').click();
         cy.get('tbody > tr').contains('cypress test').parent().contains('Edit').click();

2c5f17b5cb681de9fc8f04e9a9538d02b82ab2c3
Tomas <tomizechsterson@gmail.com>
2021-11-03 20:13:04 -0500

    Update to new local url's

--- src/ApiUrls.js
+++ src/ApiUrls.js
@@ -1,13 +1,13 @@
 export default class Urls {
     static ADD2Url = () => {
         return process.env.NODE_ENV === 'development'
-            ? 'http://localhost:43000/'
+            ? 'http://localhost:42000/'
             : 'https://add2ent-dev.azurewebsites.net/api/add2character/';
     };
 
     static DD35Url = () => {
         return process.env.NODE_ENV === 'development'
-            ? 'http://localhost:43002/'
+            ? 'http://localhost:42001/'
             : 'https://dd35ent-dev.azurewebsites.net/api/';
     }
 }

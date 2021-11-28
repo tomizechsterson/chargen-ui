a8757015b7738d6d5e51948567356a164fc2198b
Tomas <tomizechsterson@gmail.com>
2021-11-28 15:41:38 -0600

    Remove unnecessary test character properties

--- src/ADD2/__tests__/ADD2StatRoll_Tests.js
+++ src/ADD2/__tests__/ADD2StatRoll_Tests.js
@@ -60,21 +60,6 @@ describe('ADD2StatRoll Component', () => {
     con: 0,
     int: 0,
     wis: 0,
-    chr: 0,
-    race: 'none',
-    gender: 'n',
-    height: 0,
-    weight: 0,
-    age: 0,
-    className: 'none',
-    alignment: 'none',
-    paralyze: 0,
-    rod: 0,
-    petrification: 0,
-    breath: 0,
-    spell: 0,
-    hp: 0,
-    moveRate: 0,
-    funds: 0
+    chr: 0
   };
 });

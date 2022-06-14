9f7305fd94d609c15b2c13d83c371e2e9768dbf1
Tomas <tomizechsterson@users.noreply.github.com>
2022-06-13 21:14:38 -0500

    Use the global cy.mount instead of the imported one

--- cypress/component/RollOnce.cy.js
+++ cypress/component/RollOnce.cy.js
@@ -1,8 +1,7 @@
-import { mount } from 'cypress/react';
 import RollOnce from "../../src/StatRollRules/RollOnce";
 
 describe('<RollOnce />', () => {
   it('mounts', () => {
-    mount(<RollOnce />);
+    cy.mount(<RollOnce />);
   })
 })
